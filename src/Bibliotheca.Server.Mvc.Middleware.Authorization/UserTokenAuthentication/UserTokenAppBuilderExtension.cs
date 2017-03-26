d5494f40d806a5e1f66987dec01e92bb9c44286e
Marcin Czachurski <mczachurski@icloud.com>
2017-03-26 19:14:48 +0200

    Add options support.

--- src/Bibliotheca.Server.Mvc.Middleware.Authorization/UserTokenAuthentication/UserTokenAppBuilderExtension.cs
+++ src/Bibliotheca.Server.Mvc.Middleware.Authorization/UserTokenAuthentication/UserTokenAppBuilderExtension.cs
@@ -1,18 +1,24 @@
 using System;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Options;
 
 namespace Bibliotheca.Server.Mvc.Middleware.Authorization.UserTokenAuthentication
 {
     public static class UserTokenAppBuilderExtension
     {
-        public static IApplicationBuilder UseUserTokenAuthentication(this IApplicationBuilder applicationBuilder)
+        public static IApplicationBuilder UseUserTokenAuthentication(this IApplicationBuilder applicationBuilder, UserTokenOptions options)
         {
             if (applicationBuilder == null)
             {
                 throw new ArgumentNullException(nameof(applicationBuilder));
             }
 
-            return applicationBuilder.UseMiddleware<UserTokenMiddleware>();
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            return applicationBuilder.UseMiddleware<UserTokenMiddleware>(Options.Create(options));
         }
     }
 }
