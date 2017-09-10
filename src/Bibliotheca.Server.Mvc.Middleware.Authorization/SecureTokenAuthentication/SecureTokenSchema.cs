112651c33d49ccce96f694dba2b63fe1442026fe
Marcin Czachurski <mczachurski@icloud.com>
2017-09-10 11:39:16 +0200

    Fix issues with securetoken name.

--- src/Bibliotheca.Server.Mvc.Middleware.Authorization/SecureTokenAuthentication/SecureTokenSchema.cs
+++ src/Bibliotheca.Server.Mvc.Middleware.Authorization/SecureTokenAuthentication/SecureTokenSchema.cs
@@ -2,7 +2,7 @@ namespace Bibliotheca.Server.Mvc.Middleware.Authorization.SecureTokenAuthenticat
 {
     public static class SecureTokenSchema
     {
-        public static string Name { get { return "SecureToekn"; } }
+        public static string Name { get { return "SecureToken"; } }
 
         public static string Description { get { return "Secure token authorization."; } }
     }
