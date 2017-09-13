5366c683747376a6e9e3ab9a24b11581fb9a64d0
Marcin Czachurski <mczachurski@icloud.com>
2017-09-13 19:59:25 +0200

    Add new attribute for authorization.

---
+++ src/Bibliotheca.Server.Mvc.Middleware.Authorization/UserAuthorizeAttribute.cs
@@ -0,0 +1,16 @@
+using System;
+using Bibliotheca.Server.Mvc.Middleware.Authorization.SecureTokenAuthentication;
+using Bibliotheca.Server.Mvc.Middleware.Authorization.UserTokenAuthentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Bibliotheca.Server.Mvc.Middleware.Authorization
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public class UserAuthorizeAttribute : AuthorizeAttribute
+    {
+        public UserAuthorizeAttribute() : base($"{SecureTokenSchema.Name}, {UserTokenSchema.Name}, {JwtBearerDefaults.AuthenticationScheme}")
+        {
+        }
+    }
+}
