c4e57dcb0cc0a08c41ee15458c6e6c74295c0a8f
Marcin Czachurski <mczachurski@icloud.com>
2017-10-05 17:20:30 +0200

    #1 Rename method UseExceptionHandler.

--- src/Bibliotheca.Server.Mvc.Middleware.Diagnostics/Exceptions/ExceptionHandlerAppBuilderExtension.cs
+++ src/Bibliotheca.Server.Mvc.Middleware.Diagnostics/Exceptions/ExceptionHandlerAppBuilderExtension.cs
@@ -4,7 +4,12 @@ namespace Bibliotheca.Server.Mvc.Middleware.Diagnostics.Exceptions
 {
     public static class ExceptionHandlerAppBuilderExtension
     {
-        public static IApplicationBuilder UseExceptionHandler(this IApplicationBuilder builder)
+        /// <summary>
+        /// Handler which catch exceptions, logs them and returns proper Http staus code.
+        /// </summary>
+        /// <param name="builder">Application builder.</param>
+        /// <returns>Builder.</returns>
+        public static IApplicationBuilder UseErrorHandler(this IApplicationBuilder builder)
         {
             return builder.UseMiddleware<ExceptionHandlerMiddleware>();
         }
